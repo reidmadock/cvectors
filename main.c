b222f86e7e7a746dfc37496358d3cb161bc7aa88
Reid's WSL <reidmadock@gmail.com>
2023-07-18 15:30:21 -0500

    forgot how confusing C can be

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
-int main() {
-	printf("Testing");
+int* scalar_multi(int *, int);
 
-	return EXIT_SUCCESS;
+int main() {
+	printf("Testing\n");
+	int vct[] = {1,2,3,4,5,6,7,8,9,10};
+	int scl = 4;
+	int *result = scalar_multi(vct, scl);
+	// for(int i = 0; i < 2; i++) {
+		// printf("%d", result[i]);
+	//}
+	printf("result: %d \n", *result);
+	printf("result: %d \n", result[1]);
+	printf("result: %d \n", result[2]);
+	
+
+	return 0;
+}
+
+int* scalar_multi(int *vect, int scalar) {
+	int *ptr = vect;
+	int i = 0;
+	int axis_len = sizeof(vect);
+	printf("vector length: %d \n", axis_len);
+	
+	while(i < axis_len) {
+		printf("vector entry: %d \n", vect[i]);
+		vect[i++] *= scalar;
+	}
+	
+	return vect;
 }
