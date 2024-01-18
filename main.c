8b19fa6828a35a15b9391a0c0ba5e96e9a080cb8
Reid's WSL <reidmadock@gmail.com>
2024-01-18 16:01:05 -0600

    experimenting with different ways of doing this.

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int* scalar_multi(int *, int);
+void scalar_multi(int *, size_t, int);
 
 int main() {
 	printf("Testing\n");
-	int vct[10] = {1,2,3,4,5,6,7,8,9,10};
+	
+	size_t length = 10;
+	int *vct = (int*)malloc(sizeof(int) * length);
+	//int vct[10] = {1,2,3,4,5,6,7,8,9,10};
+
+	*vct = {1,2,3,4,5,6,7,8,9,10};
 	int scl = 4;
-	int *result = scalar_multi(vct, scl);
+	//
+	scalar_multi(vct, length, scl);
 	// for(int i = 0; i < 2; i++) {
 		// printf("%d", result[i]);
 	//}
@@ -15,19 +22,21 @@ int main() {
 	printf("result: %d \n", result[2]);
 	printf("Size from main %ld \n", sizeof(vct));	
 
+	free(vct);
 	return 0;
 }
 
-int* scalar_multi(int *vect, int scalar) {
+// Functional Programming???
+void scalar_multi(int *vect, size_t len, int scalar) {
+	// Point to the top of vect
 	int *ptr = vect;
 	int i = 0;
-	int axis_len = sizeof(&vect);
-	printf("vector length: %d \n", axis_len);
+	//int axis_len = sizeof(&vect);
+	//printf("vector length: %d \n", axis_len);
 	
-	while(i < axis_len) {
+	while(i < len) {
 		printf("vector entry: %d \n", vect[i]);
 		vect[i++] *= scalar;
 	}
 	
-	return vect;
 }
