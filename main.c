9af4b8ad7c08c92d3c53b56d3d8447eb6b8de639
Reid's WSL <reidmadock@gmail.com>
2024-01-18 14:10:52 -0600

    tests

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int* scalar_multi(int *, int);
 
 int main() {
 	printf("Testing\n");
-	int vct[] = {1,2,3,4,5,6,7,8,9,10};
+	int vct[10] = {1,2,3,4,5,6,7,8,9,10};
 	int scl = 4;
 	int *result = scalar_multi(vct, scl);
 	// for(int i = 0; i < 2; i++) {
@@ -13,7 +13,7 @@ int main() {
 	printf("result: %d \n", *result);
 	printf("result: %d \n", result[1]);
 	printf("result: %d \n", result[2]);
-	
+	printf("Size from main %ld \n", sizeof(vct));	
 
 	return 0;
 }
@@ -21,7 +21,7 @@ int main() {
 int* scalar_multi(int *vect, int scalar) {
 	int *ptr = vect;
 	int i = 0;
-	int axis_len = sizeof(vect);
+	int axis_len = sizeof(&vect);
 	printf("vector length: %d \n", axis_len);
 	
 	while(i < axis_len) {
